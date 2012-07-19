908e46b11044d6d7df614645be76e2e34aa4328b
Mark Kuo <mkuo@nvidia.com>
2012-07-19 15:28:22 +0800

    add a script to build cscope db for kernel tree

---
+++ build_cscope_db.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+#  Run cscope for kernel tree
+#
+#  Ref: http://naseer.in/use-cscope-to-browse-the-android-source-code
+#
+
+
+echo "Clean old database..."
+rm -f cscope.*
+
+echo "Listing kernel files..."
+LNX=$(pwd)
+
+# excluded folders here
+find .                               \
+    -path "./arch/*" -prune -o         \
+    -path "./tmp*" -prune -o           \
+    -path "./Documentation*" -prune -o \
+    -path "./scripts*" -prune -o       \
+    -path "./drivers*" -prune -o       \
+    -path "./tools*" -prune -o       \
+    -name "*.[chxsS]" -print > cscope.files
+
+# included folders under arch/
+find "./arch/arm/include/"    \
+     "./arch/arm/kernel/"     \
+     "./arch/arm/common/"     \
+     "./arch/arm/boot/"       \
+     "./arch/arm/lib/"        \
+     "./arch/arm/mm/"         \
+     "./arch/arm/mach-tegra/" \
+     -name "*.[chxsS]" -print >> cscope.files
+
+# included folders under drivers/
+find "./drivers/acpi/"  \
+     "./drivers/base/"  \
+     "./drivers/gpio/"  \
+     "./drivers/i2c/"   \
+     "./drivers/misc/"  \
+     "./drivers/power/" \
+     "./drivers/usb/"   \
+     -name "*.[chxsS]" -print >> cscope.files
+
+file_count=`wc -l cscope.files | cut -d' ' -f1`
+echo "File count:${file_count}"
+
+# create db now
+echo "Creating cscope databases..."
+time /usr/bin/cscope -bkqU
+#/usr/bin/cscope -bkqRU #regular command for all folder
+echo "Done"
+
