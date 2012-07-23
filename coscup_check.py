1a561ac5444f7d4ce8aa2ada5d5307c0f038c24b
Mark Kuo <mkuo@nvidia.com>
2012-07-23 13:09:47 +0800

    send e-mail using mutt
    so as to notify whoever has a smart phone to register in time

--- coscup_check.py
+++ coscup_check.py
@@ -12,6 +12,8 @@ from subprocess import call
 from BeautifulSoup import BeautifulSoup
 
 URL='http://registrano.com/events/coscup2012-regist'
+REGIST_URL='https://registrano.com/events/coscup2012-regist/registrations/new'
+EMAIL='starryalley@gmail.com'
 
 if __name__ == '__main__':
     try:
@@ -19,10 +21,15 @@ if __name__ == '__main__':
         res = soup.findAll('li', {'class': 'status'})
         m = re.search('尚餘(\d+)名額', str(res[0]))
         left = int(m.group(1))
+
+        # send messages
         message = "Coscup 2012 Registration: %d seat(s) left!" % left
+        mail_content= "%s\nGo to %s immediately!\n" % (message, REGIST_URL)
         print message
         if left > 0:
             call('notify-send "%s"' % message, shell = True)
+            call('echo "%s" | mutt -s "COSCUP 2012 Registration available" %s' % \
+                    (mail_content, EMAIL), shell = True)
     except:
         call('notify-send "Error checking available seats!"', shell = True)
 
