e0fc63b8e9e5510f4a7860ec2c66a409be030259
Mark Kuo <mkuo@nvidia.com>
2012-10-09 15:07:00 +0800

    endomondo is still under construction

--- auto_uploader/uploader.py
+++ auto_uploader/uploader.py
@@ -2,7 +2,7 @@
 Auto uploader
 
 Requirement:
-    apt-get install python-poster
+    apt-get install python-poster python-requests
 
 Author:
     Mark Kuo (starryalley@gmail.com)
@@ -17,9 +17,88 @@ import sys
 import argparse
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
+import uuid, socket
+import requests
 
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4"
 
 class LoginFailed(Exception):
+    pass
+
+
+# reference: https://github.com/isoteemu/sports-tracker-liberator/blob/master/endomondo.py
+# TODO: not working
+class EndomondoUploader:
+    upload_url = ''
+
+    # Some parameters what Endomondo App sends.
+    country = 'GB'
+    device_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, socket.gethostname()))
+    os = "Android"
+    app_version="7.1"
+    app_variant="M-Pro"
+    os_version="2.3.7"
+    model="HTC Vision"
+
+    # Authentication url. Special case.
+    URL_AUTH = 'https://api.mobile.endomondo.com/mobile/auth?v=2.4&action=PAIR'
+
+    def __init__(self):
+        # Using session - provides keep-alive in urllib3
+        self.Requests = requests.session()
+        # Auth token
+        self.auth_token = None
+        self.Requests.headers['User-Agent'] = "Dalvik/1.4.0 (Linux; U; %s %s; %s Build/GRI40)" % (self.os, self.os_version, self.model)
+
+
+    def request_auth_token(self, email, password):
+        params = {
+            'email':        email,
+            'password':     password,
+            'country':      self.country,
+            'deviceId':     self.device_id,
+            'os'    :       self.os,
+            'appVersion':   self.app_version,
+            'appVariant':   self.app_variant,
+            'osVersion':    self.os_version,
+            'model':        self.model
+        }
+
+        r = self.Requests.get(self.URL_AUTH, params=params)
+        lines = r.content.split("\n")
+        if lines[0] != "OK":
+            raise ValueError("Could not authenticate with Endomondo, Expected 'OK', got '%s'" % lines[0])
+
+        lines.pop(0)
+        for line in lines:
+            key, value = line.split("=")
+            if key == "authToken":
+                return value
+
+        return False
+
+    def authenticate(self, user, passwd):
+        self.auth_token = self.request_auth_token(user, passwd)
+        print self.auth_token
+
+    # Helper for generating requests - can't be used in athentication.
+    def make_request(self, url, params={}):
+        params.update({
+            'authToken': self.auth_token,
+            'language':    'EN'
+        })
+        r = self.Requests.get(url, params=params)
+
+        if r.status_code != requests.codes.ok:
+            print "Could not retrieve URL %s" % r.url
+            r.raise_for_status()
+        return r
+
+    def upload(self, f):
+        r = self.make_request("http://api.mobile.endomondo.com/mobile/api/workout/list?maxResults=5")
+        print r.content
+        pass
+
     pass
 
 
@@ -27,11 +106,10 @@ class XplovaUploader:
     auth_url = 'http://tour.xplova.com/userAccount/login.php'
     upload_url = 'http://tour.xplova.com/inc/uploadActivity.php'
 
-    def send_file(self, f):
+    def upload(self, f):
         values = {'upload_activity': f}
         data, headers = multipart_encode(values)
-        headers['User-Agent'] = \
-                'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        headers['User-Agent'] = USER_AGENT
         request = urllib2.Request(XplovaUploader.upload_url, data, headers)
         request.unverifiable = True
         print "Uploading..."
@@ -76,8 +154,11 @@ if __name__ == '__main__':
     # build argument parser
     parser = argparse.ArgumentParser(\
             description = 'Auto uploader script for sport activity sites')
-    parser.add_argument('--xplova', action = 'store_true', \
-            help = 'upload to xplova (default)', default = True)
+    group = parser.add_mutually_exclusive_group(required = True)
+    group.add_argument('--xplova', action = 'store_true', \
+            help = 'upload to xplova')
+    group.add_argument('--endomondo', action = 'store_true', \
+            help = 'upload to endomondo (not working!)')
     parser.add_argument('-i', '--import-file', required = True,\
             type = argparse.FileType('r'), \
             help = 'file to upload')
@@ -88,19 +169,25 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # do it
-    if args.xplova:
+    if args.endomondo:
+        uploader = EndomondoUploader()
+    elif args.xplova:
         uploader = XplovaUploader()
-        if args.user is None:
-            args.user = raw_input("Username: ")
-        if args.passwd is None:
-            args.passwd = getpass.getpass("Password: ")
-        try:
-            uploader.authenticate(args.user, args.passwd)
-            uploader.send_file(args.import_file)
-        except LoginFailed as e:
-            print e
-            sys.exit(1)
-        except IOError as e:
-            print e
-            sys.exit(1)
+    else:
+        print "Unknown uploader type!"
+        sys.exit(1)
+
+    if args.user is None:
+        args.user = raw_input("Username: ")
+    if args.passwd is None:
+        args.passwd = getpass.getpass("Password: ")
+    try:
+        uploader.authenticate(args.user, args.passwd)
+        uploader.upload(args.import_file)
+    except LoginFailed as e:
+        print e
+        sys.exit(1)
+    except IOError as e:
+        print e
+        sys.exit(1)
 
