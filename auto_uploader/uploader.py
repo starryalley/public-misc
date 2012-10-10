ff5ef2d0349ee58708bc12a01798102fd61f34e8
starryalley <starryalley@starryalley-laptop.(none)>
2012-10-10 21:04:03 +0800

    endomondo doesn't work for some reason.
    this commit is just to record what's done.
    Will not support uploading to endomondo.

--- auto_uploader/uploader.py
+++ auto_uploader/uploader.py
@@ -19,6 +19,8 @@ from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
 import uuid, socket
 import requests
+from StringIO import StringIO
+import gzip
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4"
 
@@ -28,7 +30,7 @@ class LoginFailed(Exception):
 
 # reference: https://github.com/isoteemu/sports-tracker-liberator/blob/master/endomondo.py
 # TODO: not working
-class EndomondoUploader:
+class EndomondoReader:
     upload_url = ''
 
     # Some parameters what Endomondo App sends.
@@ -102,15 +104,96 @@ class EndomondoUploader:
     pass
 
 
+class EndomondoUploader:
+    auth_url = "https://www.endomondo.com/access?wicket:interface=:5:pageContainer:lowerSection:lowerMain:lowerMainContent:signInPanel:signInFormPanel:signInForm::IFormSubmitListener::"
+    upload_url = "http://www.endomondo.com/?wicket:interface=:10:importPanel:wizardStepPanel:uploadForm:uploadSumbit::IActivePageBehaviorListener:0:-1&wicket:ignoreIfNotActive=true&wicket:ajax=false"
+
+    def authenticate(self, user, passwd):
+        login_data = urllib.urlencode( \
+                [('signInButton', 'x'), ('email', user), ('password', passwd)] )
+        opener = register_openers()
+        opener.add_handler(urllib2.HTTPCookieProcessor( \
+                cookielib.CookieJar()))
+        req = urllib2.Request(self.auth_url, login_data)
+        req.add_header('Referer', 'https://www.endomondo.com/access')
+        req.add_header('Origin', 'https://www.endomondo.com')
+        req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+        req.add_header('User-Agent', USER_AGENT)
+
+        #login_ret = json.loads(urllib2.urlopen(req).read())
+        login_ret = urllib2.urlopen(req).read()
+        if "Login to Endomondo" in login_ret:
+            raise LoginFailed("Login Failed!")
+        print "Login successful!"
+
+    def upload(self, f):
+
+        """
+        req = urllib2.Request("http://www.endomondo.com/workouts/")
+        req.add_header('User-Agent', USER_AGENT)
+        res = urllib2.urlopen(req)
+        ret = res.read()
+
+        with open('upload_ret.html', 'w') as f:
+            f.write(ret)
+        print ret
+
+        return
+        """
+
+        values = {'uploadFile': f, 'uploadSubmit': '1', 'id275_hf_0': ''}
+        data, headers = multipart_encode(values)
+        headers['User-Agent'] = USER_AGENT
+        headers['Origin'] = "http://www.endomondo.com"
+        headers['Referer'] = "http://www.endomondo.com/?wicket:interface=:10::::"
+        headers['Accept'] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+        headers['Accept-Encoding'] = "gzip,deflate,sdch"
+        print headers
+        req = urllib2.Request(self.upload_url, data, headers)
+        req.unverifiable = True
+        print "Uploading..."
+        response = urllib2.urlopen(req)
+        print response.info()
+        if response.info().get('Content-Encoding') == 'gzip':
+            buf = StringIO(response.read())
+            data = gzip.GzipFile(fileobj = buf)
+            ret = data.read()
+        else:
+            ret = response.read()
+
+        with open('upload_ret.html', 'w') as f:
+            f.write(ret)
+        print ret
+
+    pass
+
 class XplovaUploader:
     auth_url = 'http://tour.xplova.com/userAccount/login.php'
     upload_url = 'http://tour.xplova.com/inc/uploadActivity.php'
+
+    def authenticate(self, user, passwd):
+        login_data = urllib.urlencode( [('user', user),('pwd', passwd)] )
+
+        opener = register_openers()
+        opener.add_handler(urllib2.HTTPCookieProcessor( \
+                cookielib.CookieJar()))
+        req = urllib2.Request(self.auth_url, login_data)
+        login_ret = json.loads(urllib2.urlopen(req).read())
+
+        if login_ret['ret'] == 'RET_OK':
+            print "Login successful! UserId: %s, UserName: %s" % \
+                (login_ret["userId"], login_ret["userName"])
+            #return opener
+        else:
+            err = "Login failed [%s]. Detailed info: %s" % \
+                (login_ret['ret'], login_ret)
+            raise LoginFailed(err)
 
     def upload(self, f):
         values = {'upload_activity': f}
         data, headers = multipart_encode(values)
         headers['User-Agent'] = USER_AGENT
-        request = urllib2.Request(XplovaUploader.upload_url, data, headers)
+        request = urllib2.Request(self.upload_url, data, headers)
         request.unverifiable = True
         print "Uploading..."
         response = urllib2.urlopen(request)
@@ -129,24 +212,6 @@ class XplovaUploader:
             print "Upload successful!"
         print "Full response:", ret
 
-
-    def authenticate(self, user, passwd):
-        login_data = urllib.urlencode( [('user', user),('pwd', passwd)] )
-
-        opener = register_openers()
-        opener.add_handler(urllib2.HTTPCookieProcessor( \
-                cookielib.CookieJar()))
-        req = urllib2.Request(XplovaUploader.auth_url, login_data)
-        login_ret = json.loads(urllib2.urlopen(req).read())
-
-        if login_ret['ret'] == 'RET_OK':
-            print "Login successful! UserId: %s, UserName: %s" % \
-                (login_ret["userId"], login_ret["userName"])
-            return opener
-        else:
-            err = "Login failed [%s]. Detailed info: %s" % \
-                (login_ret['ret'], login_ret)
-            raise LoginFailed(err)
     pass
 
 
